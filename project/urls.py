9af9c472fac0cc5469625bc829aee5f46c88a1d9
Katynl <katytun30101981@gmail.com>
2026-06-24 15:15:34 +1000

    update

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.urls')),   # подключаем маршруты из приложения app
+    path('', include('app.urls')),   # подключаем маршруты из приложения app
 ]
 
 # Для отображения загруженных картинок во время разработки
